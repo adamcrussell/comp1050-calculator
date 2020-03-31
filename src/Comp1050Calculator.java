c14961a2457b750052da33d252f49335f9346583
Adam Russell <ac.russell@live.com>
2020-03-31 11:51:12 -0400

    Update Comp1050Calculator.java

--- src/Comp1050Calculator.java
+++ src/Comp1050Calculator.java
@@ -18,12 +18,25 @@ import java.util.ArrayList;
 public class Comp1050Calculator extends Application implements Initializable {
 	@FXML
 	Button button1;
+	@FXML
+	Button button2;
+	@FXML
+	Button button3;
+	@FXML
+	Button button4;
+	@FXML
+	Button button5;
+	@FXML
+	Button buttonAdd;
+	@FXML
+	Button buttonEquals;
 
 	@FXML
 	TextField numberId;
 	
 	private String currentNumber=new String();
 	private ArrayList<Integer>numbers = new ArrayList<Integer>();
+	
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -43,7 +56,55 @@ public class Comp1050Calculator extends Application implements Initializable {
 				numberId.setText(currentNumber);
 			}
 		});
+		button2.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentNumber+="2";
+				numberId.setText(currentNumber);
+			}
+		});
+		button3.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentNumber+="3";
+				numberId.setText(currentNumber);
+			}
+		});
+		button4.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentNumber+="4";
+				numberId.setText(currentNumber);
+			}
+		});
+		button5.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				currentNumber+="5";
+				numberId.setText(currentNumber);
+			}
+		});
+		buttonAdd.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				numbers.add(Integer.parseInt(currentNumber));
+				currentNumber="";
+				numberId.setText(currentNumber);
+			}
+		});
+		buttonEquals.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				numbers.add(Integer.parseInt(currentNumber));
+				currentNumber="";
+				numberId.setText(currentNumber);
+				int answer = numbers.get(0) + numbers.get(1);
+				currentNumber += answer;
+				numberId.setText(currentNumber);
+			}
+		});
 	}
+	
 	
 	public static void main(String[] args) {
 		launch(args);
